4a18559707b31c16fb0d6c47ac271cf3c89d3910
valdiev <valdiev@live.fr>
2023-02-24 16:18:10 +0100

    setup sign up feature

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,18 @@
-import * as firebase from 'firebase';
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCHDs2Lvuhy9qm946ZxwrIQgL4tbqgV4eA',
   authDomain: 'knowledge-cf905.firebaseapp.com',
-  databaseURL: 'https://your-database-name.firebaseio.com',
   projectId: 'knowledge-cf905',
   storageBucket: 'knowledge-cf905.appspot.com',
   messagingSenderId: '535836127704',
   appId: '1:535836127704:web:9bd62198c3b4cbac3942ad',
 };
-let app;
-if (firebase.apps.length === 0) {
-    app = firebase.initializeApp(firebaseConfig);
-} else {
-    app = firebase.app();
-}
-const auth = firebase.auth();
 
-export { firebase };
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase }
